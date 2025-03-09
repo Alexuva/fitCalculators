2c79ea8b2e87969ee0dd9af4f1bcceafb8aedf7e
Alexuva <alejandrohernandezrosa@gmail.com>
2025-03-09 18:25:20 +0100

    First commit

---
+++ test_fuerza_tren_inferior/index.js
@@ -0,0 +1,162 @@
+class LowerBodyTest {
+
+    strengthLvl_simple = {
+        "men": {
+          "16-25": {
+            "veryLow": { "max": null, "min": 25 },
+            "low": { "max": 30, "min": 25 },
+            "somewhatLow": { "max": 34, "min": 31 },
+            "normal": { "max": 38, "min": 35 },
+            "good": { "max": 43, "min": 39 },
+            "veryGood": { "max": 49, "min": 44 },
+            "excellent": { "max": 49, "min": null }
+          },
+          "26-35": {
+            "veryLow": { "max": null, "min": 22 },
+            "low": { "max": 27, "min": 22 },
+            "somewhatLow": { "max": 31, "min": 28 },
+            "normal": { "max": 35, "min": 32 },
+            "good": { "max": 39, "min": 36 },
+            "veryGood": { "max": 45, "min": 40 },
+            "excellent": { "max": 45, "min": null }
+          },
+          "36-45": {
+            "veryLow": { "max": null, "min": 17 },
+            "low": { "max": 22, "min": 17 },
+            "somewhatLow": { "max": 26, "min": 23 },
+            "normal": { "max": 30, "min": 27 },
+            "good": { "max": 34, "min": 31 },
+            "veryGood": { "max": 41, "min": 35 },
+            "excellent": { "max": 41, "min": null }
+          },
+          "46-55": {
+            "veryLow": { "max": null, "min": 13 },
+            "low": { "max": 17, "min": 13 },
+            "somewhatLow": { "max": 21, "min": 18 },
+            "normal": { "max": 25, "min": 22 },
+            "good": { "max": 29, "min": 26 },
+            "veryGood": { "max": 35, "min": 30 },
+            "excellent": { "max": 35, "min": null }
+          },
+          "56-65": {
+            "veryLow": { "max": null, "min": 9 },
+            "low": { "max": 12, "min": 9 },
+            "somewhatLow": { "max": 16, "min": 13 },
+            "normal": { "max": 20, "min": 17 },
+            "good": { "max": 24, "min": 21 },
+            "veryGood": { "max": 30, "min": 25 },
+            "excellent": { "max": 30, "min": null }
+          },
+          "66-75": {
+            "veryLow": { "max": null, "min": 7 },
+            "low": { "max": 10, "min": 7 },
+            "somewhatLow": { "max": 14, "min": 11 },
+            "normal": { "max": 18, "min": 15 },
+            "good": { "max": 22, "min": 19 },
+            "veryGood": { "max": 28, "min": 23 },
+            "excellent": { "max": 28, "min": null }
+          },
+          "75+": {
+            "veryLow": { "max": null, "min": 5 },
+            "low": { "max": 7, "min": 5 },
+            "somewhatLow": { "max": 11, "min": 8 },
+            "normal": { "max": 15, "min": 12 },
+            "good": { "max": 20, "min": 16 },
+            "veryGood": { "max": 25, "min": 21 },
+            "excellent": { "max": 25, "min": null }
+          }
+        },
+        "women": {
+          "16-25": {
+            "veryLow": { "max": null, "min": 18 },
+            "low": { "max": 24, "min": 18 },
+            "somewhatLow": { "max": 28, "min": 25 },
+            "normal": { "max": 33, "min": 29 },
+            "good": { "max": 37, "min": 34 },
+            "veryGood": { "max": 43, "min": 38 },
+            "excellent": { "max": 43, "min": null }
+          },
+          "26-35": {
+            "veryLow": { "max": null, "min": 15 },
+            "low": { "max": 20, "min": 15 },
+            "somewhatLow": { "max": 26, "min": 21 },
+            "normal": { "max": 31, "min": 27 },
+            "good": { "max": 35, "min": 32 },
+            "veryGood": { "max": 39, "min": 35 },
+            "excellent": { "max": 39, "min": null }
+          },
+          "36-45": {
+            "veryLow": { "max": null, "min": 12 },
+            "low": { "max": 16, "min": 12 },
+            "somewhatLow": { "max": 21, "min": 17 },
+            "normal": { "max": 27, "min": 22 },
+            "good": { "max": 32, "min": 28 },
+            "veryGood": { "max": 36, "min": 33 },
+            "excellent": { "max": 36, "min": null }
+          },
+          "46-55": {
+            "veryLow": { "max": null, "min": 7 },
+            "low": { "max": 13, "min": 7 },
+            "somewhatLow": { "max": 18, "min": 14 },
+            "normal": { "max": 22, "min": 19 },
+            "good": { "max": 26, "min": 23 },
+            "veryGood": { "max": 33, "min": 27 },
+            "excellent": { "max": 33, "min": null }
+          },
+          "56-65": {
+            "veryLow": { "max": null, "min": 5 },
+            "low": { "max": 10, "min": 5 },
+            "somewhatLow": { "max": 14, "min": 11 },
+            "normal": { "max": 18, "min": 15 },
+            "good": { "max": 22, "min": 19 },
+            "veryGood": { "max": 27, "min": 23 },
+            "excellent": { "max": 27, "min": null }
+          },
+          "66-75": {
+            "veryLow": { "max": null, "min": 3 },
+            "low": { "max": 6, "min": 3 },
+            "somewhatLow": { "max": 9, "min": 7 },
+            "normal": { "max": 12, "min": 10 },
+            "good": { "max": 17, "min": 13 },
+            "veryGood": { "max": 24, "min": 18 },
+            "excellent": { "max": 24, "min": null }
+          },
+          "75+": {
+            "veryLow": { "max": null, "min": 2 },
+            "low": { "max": 4, "min": 2 },
+            "somewhatLow": { "max": 8, "min": 5 },
+            "normal": { "max": 13, "min": 9 },
+            "good": { "max": 16, "min": 14 },
+            "veryGood": { "max": 23, "min": 17 },
+            "excellent": { "max": 23, "min": null }
+          }
+        }
+    }
+
+    evaluateStrenght_simple(gender, age, score){
+        const ageRanges = Object.keys(this.strengthLvl_simple[gender]);
+        const ageRange = ageRanges.find(range => {
+            const [min, max] = range.split('-');
+            return (age >= parseInt(min) && (max ? age <= parseInt(max) : true))
+        })
+
+        const levels = this.strengthLvl_simple[gender][ageRange];
+        for(const level in levels){
+            const {min, max} = levels[level];
+            if(max === null && score < min){
+                return level;
+            }else if(min === null && score > max){
+                return level;
+            }else if(score >= min && score <= max){
+                return level;
+            }
+        }
+        return levels;
+    }
+
+}
+
+
+let calculator = new LowerBodyTest();
+let level = calculator.evaluateStrenght_simple("women", 75, 2);
+console.log(level);
