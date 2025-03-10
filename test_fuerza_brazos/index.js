ad9ffcf8068b0662e43ec0736d4fe63ed36486fc
Alejandro Hernández Rosa <alejandrohernandezrosa@gmail.com>
2025-03-10 20:10:00 +0100

    strength schema and level

--- test_fuerza_brazos/index.js
+++ test_fuerza_brazos/index.js
@@ -2,6 +2,7 @@
  * Calculadora de fuerza de los brazos
  * Datos necesarios: 
  * - Sexo: Hombre o mujer
+ * - Edad: minimo 16
  * - Peso corporal Kg
  * - Conoces tu 1RM? Si o No
  * - 1RM Kg (Si conoces)
@@ -15,6 +16,7 @@
 class ArmsStrenght {
 
     _gender;
+    _age;
     _bodyWeight;
     _maxWeight = null;
     _weight_1;
@@ -24,9 +26,126 @@ class ArmsStrenght {
     _mayhew;
     _wathan;
     _relativeStrenght;
+    _strengthLevel;
 
-    constructor(gender, bodyWeight, maxWeight, weight_1, weight_2, reps_1, reps_2){
+    _strengthLevels = {
+        "men": {
+            "16-25": {
+                "veryLow": { "max": null, "min": 0.10 },
+                "low": { "max": 0.14, "min": 0.10 },
+                "somewhatLow": { "max": 0.19, "min": 0.15 },
+                "normal": { "max": 0.29, "min": 0.20 },
+                "good": { "max": 0.39, "min": 0.30 },
+                "veryGood": { "max": 0.50, "min": 0.40 },
+                "excellent": { "max": 0.50, "min": null }
+            },
+            "26-35": {
+                "veryLow": { "max": null, "min": 0.11 },
+                "low": { "max": 0.15, "min": 0.11 },
+                "somewhatLow": { "max": 0.21, "min": 0.16 },
+                "normal": { "max": 0.31, "min": 0.22 },
+                "good": { "max": 0.43, "min": 0.32 },
+                "veryGood": { "max": 0.55, "min": 0.44 },
+                "excellent": { "max": 0.55, "min": null }
+            },
+            "36-45": {
+                "veryLow": { "max": null, "min": 0.10 },
+                "low": { "max": 0.14, "min": 0.10 },
+                "somewhatLow": { "max": 0.19, "min": 0.15 },
+                "normal": { "max": 0.29, "min": 0.20 },
+                "good": { "max": 0.39, "min": 0.30 },
+                "veryGood": { "max": 0.50, "min": 0.40 },
+                "excellent": { "max": 0.50, "min": null }
+            },
+            "46-55": {
+                "veryLow": { "max": null, "min": 0.09 },
+                "low": { "max": 0.12, "min": 0.09 },
+                "somewhatLow": { "max": 0.17, "min": 0.13 },
+                "normal": { "max": 0.27, "min": 0.18 },
+                "good": { "max": 0.37, "min": 0.28 },
+                "veryGood": { "max": 0.45, "min": 0.38 },
+                "excellent": { "max": 0.45, "min": null }
+            },
+            "56-65": {
+                "veryLow": { "max": null, "min": 0.08 },
+                "low": { "max": 0.11, "min": 0.08 },
+                "somewhatLow": { "max": 0.15, "min": 0.12 },
+                "normal": { "max": 0.24, "min": 0.16 },
+                "good": { "max": 0.31, "min": 0.25 },
+                "veryGood": { "max": 0.40, "min": 0.32 },
+                "excellent": { "max": 0.40, "min": null }
+            },
+            ">65": {
+                "veryLow": { "max": null, "min": 0.07 },
+                "low": { "max": 0.10, "min": 0.07 },
+                "somewhatLow": { "max": 0.14, "min": 0.11 },
+                "normal": { "max": 0.21, "min": 0.15 },
+                "good": { "max": 0.27, "min": 0.22 },
+                "veryGood": { "max": 0.32, "min": 0.28 },
+                "excellent": { "max": 0.32, "min": null }
+            }
+        },
+        "women": {
+            "16-25": {
+                "veryLow": { "max": null, "min": 0.07 },
+                "low": { "max": 0.10, "min": 0.07 },
+                "somewhatLow": { "max": 0.14, "min": 0.11 },
+                "normal": { "max": 0.21, "min": 0.15 },
+                "good": { "max": 0.27, "min": 0.22 },
+                "veryGood": { "max": 0.32, "min": 0.28 },
+                "excellent": { "max": 0.32, "min": null }
+            },
+            "26-35": {
+                "veryLow": { "max": null, "min": 0.08 },
+                "low": { "max": 0.11, "min": 0.08 },
+                "somewhatLow": { "max": 0.15, "min": 0.12 },
+                "normal": { "max": 0.24, "min": 0.16 },
+                "good": { "max": 0.31, "min": 0.25 },
+                "veryGood": { "max": 0.40, "min": 0.32 },
+                "excellent": { "max": 0.40, "min": null }
+            },
+            "36-45": {
+                "veryLow": { "max": null, "min": 0.07 },
+                "low": { "max": 0.10, "min": 0.07 },
+                "somewhatLow": { "max": 0.14, "min": 0.11 },
+                "normal": { "max": 0.21, "min": 0.15 },
+                "good": { "max": 0.27, "min": 0.22 },
+                "veryGood": { "max": 0.32, "min": 0.28 },
+                "excellent": { "max": 0.32, "min": null }
+            },
+            "46-55": {
+                "veryLow": { "max": null, "min": 0.05 },
+                "low": { "max": 0.08, "min": 0.05 },
+                "somewhatLow": { "max": 0.12, "min": 0.09 },
+                "normal": { "max": 0.18, "min": 0.13 },
+                "good": { "max": 0.24, "min": 0.19 },
+                "veryGood": { "max": 0.30, "min": 0.25 },
+                "excellent": { "max": 0.30, "min": null }
+            },
+            "56-65": {
+                "veryLow": { "max": null, "min": 0.03 },
+                "low": { "max": 0.07, "min": 0.03 },
+                "somewhatLow": { "max": 0.10, "min": 0.07 },
+                "normal": { "max": 0.14, "min": 0.11 },
+                "good": { "max": 0.21, "min": 0.15 },
+                "veryGood": { "max": 0.27, "min": 0.22 },
+                "excellent": { "max": 0.27, "min": null }
+            },
+            ">65": {
+                "veryLow": { "max": null, "min": 0.02 },
+                "low": { "max": 0.04, "min": 0.02 },
+                "somewhatLow": { "max": 0.08, "min": 0.05 },
+                "normal": { "max": 0.12, "min": 0.09 },
+                "good": { "max": 0.18, "min": 0.13 },
+                "veryGood": { "max": 0.24, "min": 0.19 },
+                "excellent": { "max": 0.24, "min": null }
+            }
+        }
+    }
+
+    constructor(gender, age, bodyWeight, maxWeight, weight_1, weight_2, reps_1, reps_2){
         this._gender = gender;
+        this._age = age;
         this._maxWeight = maxWeight;
         this._weight_1 = weight_1;
         this._weight_2 = weight_2;
@@ -61,8 +180,34 @@ class ArmsStrenght {
         return this._relativeStrenght;
     }
 
+    get StrengthLevel(){
+        const ageRanges = Object.keys(this._strengthLevels[this._gender]);
+        const ageRange = ageRanges.find(range => {
+            if (range.startsWith('>')) {
+                return this._age > parseInt(range.substring(1));
+            } else {
+                const [min, max] = range.split('-');
+                return (this._age >= parseInt(min) && this._age <= parseInt(max));
+            }
+        });
+
+        const levels = this._strengthLevels[this._gender][ageRange];
+        for (const level in levels) {
+            const { min, max } = levels[level];
+            if (max === null && this._relativeStrenght < min) {
+                return level;
+            } else if (min === null && this._relativeStrenght > max) {
+                return level;
+            } else if (this._relativeStrenght >= min && this._relativeStrenght <= max) {
+                return level;
+            }
+        }
+        
+    }
+
 }
 
-const calculator = new ArmsStrenght('men', 90, null, 40, 38, 3, 4);
+const calculator = new ArmsStrenght('men', 31, 90, null, 5, 5, 3, 4);
 console.log(calculator.WithoutRM);
 console.log(calculator.RelativeStrenght);
+console.log(calculator.StrengthLevel);
